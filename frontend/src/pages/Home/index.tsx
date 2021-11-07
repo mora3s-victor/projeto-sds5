bb91a9d427ddb8dfbc55da077f0a0b8d576f792a
victor moraes <victorhardflipmoraes@outlook.com>
2021-11-07 00:59:17 -0400

    Routes

---
+++ frontend/src/pages/Home/index.tsx
@@ -0,0 +1,12 @@
+//essa função é uma variável, uma expressão lambda
+const Home = () => {
+    return (
+        <div>
+            <h1>
+                Pagina home
+            </h1>
+        </div>
+    );
+}
+
+export default Home;
